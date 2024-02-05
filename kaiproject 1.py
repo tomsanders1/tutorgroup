fb1e08827af870d5455eca787afd348f50831fe5
tomsanders1 <145353090+tomsanders1@users.noreply.github.com>
2024-02-05 12:31:10 +0000

    Add files via upload

---
+++ kaiproject 1.py
@@ -0,0 +1,62 @@
+import csv
+
+
+
+def LoginPage():
+    while True:
+        username = input("Enter username: ")
+        password = input("Enter password: ")
+        if username == "admin" and password == "SecurePass":
+            print("Login successful")
+            return True
+        else:
+            print("Incorrect username or password. Please try again.")
+
+
+
+def EnterDetails():
+    with open("Students.csv", mode='a', newline='') as file:
+        writer = csv.writer(file)
+        print("\nEnter student details:")
+        UniqueID = str(input("Enter Student ID: "))
+        Surname = str(input("Enter Surname: "))
+        Forename = str(input("Enter Forename; "))
+        DateOfBirth = str(input("Enter Date Of Birth: "))
+        HomeAddress = str(input("Enter Home Address: "))
+        PhoneNum = str(input("Enter Phone Number: "))
+        Gender = str(input("Enter Gender: "))
+        Tutor = str(input("Enter Tutor Group: "))
+
+        writer.writerow([UniqueID, Surname, Forename, DateOfBirth, HomeAddress, PhoneNum, Gender, Tutor])
+        print("Student Details Added")
+
+def ViewDetails():
+    try:
+        with open("Students.csv", mode='r') as file:
+            reader = csv.reader(file)
+            print("\nStudent details:")
+            for row in reader:
+                print(row)
+    except FileNotFoundError:
+        print("No student details found.")
+
+def Menu():
+    if LoginPage():
+        while True:   
+            print("Menu:")
+            print("1) Enter Student Details.")
+            print("2) View Student Details.")
+            print("3) Log Out.")
+            Option = input("Select Option: ")
+            if Option == "1":
+                EnterDetails()
+            elif Option == "2":
+                ViewDetails()
+            elif Option == "3":
+                print("Logged out.")
+                break
+            else:
+                print("Invalid Option. Please enter 1, 2, or 3.")
+
+if __name__ == "__main__":
+    Menu()
